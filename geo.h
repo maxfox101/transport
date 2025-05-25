9598d120ec17f0d52c7e4200ec4f5f4b5726be0f
maxfox101 <maks280706@gmail.com>
2025-05-26 00:44:14 +0300

    Update geo.h

--- geo.h
+++ geo.h
@@ -5,30 +5,21 @@
 struct Coordinates {
     double lat;
     double lng;
-
     bool operator==(const Coordinates& other) const {
         return lat == other.lat && lng == other.lng;
+    }
+    bool operator!=(const Coordinates& other) const {
+        return !(*this == other);
     }
 };
 
 inline double ComputeDistance(Coordinates from, Coordinates to) {
+    using namespace std;
     if (from == to) {
         return 0;
     }
-
-    const double R = 6371000;
-    const double deg_to_rad = M_PI / 180.0;
-
-    double lat1 = from.lat * deg_to_rad;
-    double lat2 = to.lat * deg_to_rad;
-    double delta_lat = (to.lat - from.lat) * deg_to_rad;
-    double delta_lng = (to.lng - from.lng) * deg_to_rad;
-
-    double a = std::sin(delta_lat / 2) * std::sin(delta_lat / 2)
-               + std::cos(lat1) * std::cos(lat2)
-                     * std::sin(delta_lng / 2) * std::sin(delta_lng / 2);
-
-    double c = 2 * std::atan2(std::sqrt(a), std::sqrt(1 - a));
-
-    return R * c;
+    static const double dr = 3.1415926535 / 180.;
+    return acos(sin(from.lat * dr) * sin(to.lat * dr)
+                + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
+        * 6371000;
 }
