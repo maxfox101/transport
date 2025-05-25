8ee851a95ffdd76f32dd588d764e01db605fbca7
maxfox101 <maks280706@gmail.com>
2025-05-26 00:43:41 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,34 +1,32 @@
 #include <iostream>
 #include <string>
-#include <string_view>
 
 #include "input_reader.h"
 #include "stat_reader.h"
+
+using namespace std;
 
 int main() {
     TransportCatalogue catalogue;
 
     int base_request_count;
-    std::cin >> base_request_count >> std::ws;
+    cin >> base_request_count >> ws;
 
     {
         InputReader reader;
         for (int i = 0; i < base_request_count; ++i) {
-            std::string line;
-            std::getline(std::cin >> std::ws, line);
+            string line;
+            getline(cin, line);
             reader.ParseLine(line);
         }
         reader.ApplyCommands(catalogue);
     }
 
     int stat_request_count;
-    std::cin >> stat_request_count >> std::ws;
-
+    cin >> stat_request_count >> ws;
     for (int i = 0; i < stat_request_count; ++i) {
-        std::string line;
-        std::getline(std::cin >> std::ws, line);
-        ParseAndPrintStat(catalogue, line, std::cout);
+        string line;
+        getline(cin, line);
+        ParseAndPrintStat(catalogue, line, cout);
     }
-
-    return 0;
 }
